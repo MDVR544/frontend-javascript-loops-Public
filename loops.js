1f6a0da3708da1622af7c116a641f11f543a78c0
MDVR544 <devriescool@gmail.com>
2026-03-17 13:19:03 +0100

    uitwerking opdracht 1

--- loops.js
+++ loops.js
@@ -10,6 +10,11 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen... Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+const starIcon = "*";
+
+for (let i = 0; i < 6; i++) {
+    console.log(starIcon.repeat(i))
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -20,6 +25,7 @@
 // loop...
 // klaar!
 // ==========================================
+
 for (let i = 0; i < 5; i++) {
     if (i < 4) {
         console.log("loop...");
@@ -27,6 +33,7 @@ for (let i = 0; i < 5; i++) {
         console.log("klaar!");
     }
 }
+
 // ==========================================
 // Opdracht 3. Maak een for-loop die automatisch factuurnummers genereert in het formaat INV-0001, INV-0002, enzovoorts. Zorg ervoor dat je begint  bij nummer 1 en dat de nummers oplopen.Er zijn in totaal 8 factuurnummers nodig.
 
