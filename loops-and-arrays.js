c3055e6a371b8638547e92d5becde6f8feb03d73
MDVR544 <devriescool@gmail.com>
2026-03-17 16:06:16 +0100

    uitwerking loops-and-arrays.js klaar

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -13,6 +13,9 @@
 
 const meetingTimes = ['09:00', '10:30', '14:00', '15:30', '17:00'];
 
+for (let i = 0; i < meetingTimes.length; i++) {
+    console.log("Vergadering om " + meetingTimes[i]);
+}
 
 // ==========================================
 // Opdracht 2. In tegenstelling tot Bob, werken de andere medewerkers van Loop-it Solutions wél hard.
@@ -27,6 +30,10 @@ const meetingTimes = ['09:00', '10:30', '14:00', '15:30', '17:00'];
 
 const salaries = [3200, 2350, 2800, 3500, 2800];
 
+for (let i = 0; i < salaries.length; i++) {
+    salaries[i] = salaries[i] * 1.05
+console.log(salaries[i]);
+}
 
 // ==========================================
 // Opdracht 3. Eén van Bob's taken is medewerkers feliciteren met hun "zoveelste" verjaardag.
@@ -38,7 +45,12 @@ const salaries = [3200, 2350, 2800, 3500, 2800];
 // ==========================================
 
 const birthYears = [1995, 1997, 1990, 2003, 1982];
+const currentYear = 2025
 
+for (let i = 0; i < birthYears.length; i++) {
+    birthYears[i] = currentYear - birthYears[i]
+    console.log(birthYears[i])
+}
 
 // ==========================================
 // Opdracht 4. Bob houdt bij hoeveel verlofuren medewerkers per maand opnemen. Nu wil hij voor het nieuwe jaar een bonusstructuur toepassen:
@@ -51,6 +63,16 @@ const birthYears = [1995, 1997, 1990, 2003, 1982];
 // ==========================================
 
 const leaveHours = [6, 9, 2, 7, 3];
+
+for (let i = 0; i < leaveHours.length; i++) {
+    if (leaveHours[i] % 2 === 0) {
+        leaveHours[i] = leaveHours[i] * 2;
+    }
+        else {
+            leaveHours[i] = leaveHours[i] * 0.5;
+        }
+    console.log(leaveHours[i]);
+}
 
 
 // ==========================================
